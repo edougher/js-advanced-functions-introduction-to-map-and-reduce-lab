3c8d29ca8bbfe559e3cb5d48ee40c48398fd8adf
edougher <ejdougher@gmail.com>
2021-02-14 23:27:40 -0600

    commit tree dafeb748be3d3dffb93ea2ef591a51a3cd9d8dff

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Your code here
+function mapToNegativize(arr) {
+    let r = []
+    for (let i = 0; i < arr.length; i++) {
+        r.push(-1 * arr[i])
+    }
+    return r
+}
+
+function mapToNoChange(arr){
+    let r = []
+    for (let i = 0; i < arr.length; i++){
+    r.push(arr[i])
+    }
+    return r
+}
+
+function mapToDouble(arr){
+    let f = []
+    for (let i = 0; i < arr.length; i++){
+        f.push(2 * arr[i])
+    }
+    return f
+}
+
+function mapToSquare(arr){
+    let f = []
+    for (let i = 0; i < arr.length; i++){
+        f.push( arr[i] * arr[i])
+    }
+    return f
+}
+
+function reduceToTotal( arr, startPoint=0){
+    let total = startPoint
+    for (let i = 0; i < arr.length; i++){
+        total = total + arr[1]
+    }
+    return total
+}
+
+function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+
+function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
+
+
+
